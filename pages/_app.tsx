a91a11aedd69b7f02468d1c47830bc182f91ab94
John Evert Muring <muring.evert@yahoo.com>
2022-09-15 18:34:56 +0800

    3rd commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return ( <div className='m-5'> <Header /> <Component {...pageProps} />
+  return ( <div className='m-5'> <Header /> <SessionProvider session={pageProps.session}><Component  {...pageProps} /></SessionProvider>
   <Footer /> </div>)
 }
 
